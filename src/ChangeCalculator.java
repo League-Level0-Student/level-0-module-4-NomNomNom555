e56861ac9583c32679b24fe0e141a1908282a12e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-13 19:23:44 -0700

    change calucators fruit quiz clapping.wav

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -16,16 +16,20 @@ public class ChangeCalculator {
 		// Ask the user how many nickels they have
 String nick= JOptionPane.showInputDialog("How many nickles do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-int five=Integer.parseInt("nick");
+int five=Integer.parseInt(nick);
 		// Ask the user how many dimes they have, and convert their answer
-String quart=JOptionPane.showInputDialog("How many quarters do you have?");
-int twentyfive=Integer.parseInt("quart");
+String dim=JOptionPane.showInputDialog("How many dimes do you have?");
+int ten=Integer.parseInt(dim);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quart=JOptionPane.showInputDialog("How many quarters do you have?");
+int twentyfive=Integer.parseInt(quart);
 		// Calculate how much money the user has and save it in a double variable 
-
+double nickels= five*.05;
+double dimes= ten*.1;
+double quarters=twentyfive*.25;
 		// Tell the user how much money they have
-
+	double all= nickels+dimes+quarters;
+	JOptionPane.showMessageDialog(null, "You have "+all+" cents");
 	}
 }
 
