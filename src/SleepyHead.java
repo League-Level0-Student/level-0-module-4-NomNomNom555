062d6fcb2c8a962c93d03192e933ed3cd8c97d18
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-03 19:22:11 -0700

    sleepyhead happy pet

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -16,12 +16,40 @@ public class SleepyHead {
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    int sleep=JOptionPane.showConfirmDialog(null, "Is it a week day?","",JOptionPane.YES_NO_OPTION);
-System.out.println(sleep);
+     
+    int fundays=JOptionPane.showConfirmDialog(null, "Is it a weekend?","",JOptionPane.YES_NO_OPTION);   
+    System.out.println(fundays);
+   if(fundays==0) {
+    JOptionPane.showMessageDialog(null,"Sleep in.");	
+    }
+   if(fundays==1) { 
+	   int sleep=JOptionPane.showConfirmDialog(null, "Is it a week day?","",JOptionPane.YES_NO_OPTION);
+	    System.out.println(sleep);
+	    if(sleep==0) {
+	    int TenHP=JOptionPane.showConfirmDialog(null, "Are you on vacation?","",JOptionPane.YES_NO_OPTION);
+	    System.out.println(TenHP);
+	    if(TenHP==1) {
+	    JOptionPane.showMessageDialog(null,"Get up you lazybones!");	
+	    }
+	    if(TenHP==0) {
+	    JOptionPane.showMessageDialog(null, "Sleep in.");
+	    }
+	    }
+	    if(sleep==1) {
+	    	JOptionPane.showMessageDialog(null,"You're lying! You said it wasn't a weekend either!");
+	   }
+
+   }
+   
+  
         /*
+    
+    
+         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
+    
     }
 }
