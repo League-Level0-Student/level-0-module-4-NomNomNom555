ea81fe7f7689a40b01b262df10d8e7cdb31f023e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-14 10:55:22 -0700

    happy pet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -11,6 +11,8 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		String	PetStore=JOptionPane.showInputDialog("What pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 6; i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
@@ -20,10 +22,29 @@ public class HappyPet {
 					new String[] { "Feed it", "Give it excercise/play with it", "Give it love and attention", "All of them" }, null);
 				System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-
+				if(task==0) {
+					feed();
+				}
+				
+				if(task==1){
+					move();
+				}
+				
+				if(task==2) {
+					love();	
+				}
+				
+				if(task==3) {
+					all();
+				}
+				
 			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
-
+				//    user that he loves his pet and use break; to exit for loop.
+				if(happinessLevel>=6) {
+				JOptionPane.showMessageDialog(null, "You love your pet!");
+					break;
+				}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
@@ -31,9 +52,23 @@ public class HappyPet {
 	//    and INCREMENT the pet's happiness Level.
 	
 	static void feed() {
-	
-	
+	JOptionPane.showMessageDialog(null,"Woof Woof!");
+	happinessLevel++;
 	}
 	
+	static void move() {
+	JOptionPane.showMessageDialog(null, "Purrrrrrr!");
+	happinessLevel+=2;
+	}
 	
-}
+	static void love() {
+	JOptionPane.showMessageDialog(null, "Squeak, squeak!");	
+	happinessLevel+=3;
+	}
+	
+	static void all() {
+	JOptionPane.showMessageDialog(null, "Woof Woof Woof Woof Woof Woof Woof Woof Woof!");
+	happinessLevel+=6;
+	}
+	
+	}
