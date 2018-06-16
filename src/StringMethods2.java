948396784c82785e7615ab9ec9aced151cf8fea2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-16 10:55:27 -0700

    string method two

--- src/StringMethods2.java
+++ src/StringMethods2.java
@@ -7,18 +7,22 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+		String two= "AbcdEfghIjklmnOpqrstUvwxYz";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(two.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
-		// 4. Print the first 3 char's of your String
-		//    HINT: .substring(start,end)
-		
+		System.out.println(two.toLowerCase());
+		// 4. Print out the first three chars (substring)
+		System.out.println(two.substring(0,3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		char line= two.charAt(12);
+		System.out.println(line);
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		
+		two.substring(two.length());
+		System.out.println(two.substring(two.length()-3,two.length()));
 	}
 }
+ 
